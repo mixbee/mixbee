c58efb28297e6b83a876353428537a0a8e5ecae0
eleven <jianlongcui23@gmail.com>
2018-07-24 11:49:28 +0800

    config, test , docker

--- example/stop.sh
+++ example/stop.sh
@@ -11,5 +11,8 @@ rm -rf ./config/node1/Chain ./config/node1/Log ./config/node1/peers.recent
 rm -rf ./config/node2/Chain ./config/node2/Log ./config/node2/peers.recent
 rm -rf ./config/node3/Chain ./config/node3/Log ./config/node3/peers.recent
 rm -rf ./config/node4/Chain ./config/node4/Log ./config/node4/peers.recent
+rm -rf ./config/node5/Chain ./config/node5/Log ./config/node5/peers.recent
+
+docker rm -f  $(docker ps -a -q)
 
 
