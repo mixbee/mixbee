9635cc12372e3e8d4bbeb912f2114258e3506932
eleven <jianlongcui23@gmail.com>
2018-07-25 11:45:41 +0800

    example name fix

--- example/VBFT_example/start.sh
+++ example/VBFT_example/start.sh
@@ -9,4 +9,4 @@ set -ev
 docker-compose -f docker-compose.yml down
 
 
-docker-compose -f docker-compose.yml up -d node1.example.com node2.example.com node3.example.com node4.example.com node5.example.com
+docker-compose -f docker-compose.yml up -d node1.example.com node2.example.com node3.example.com node4.example.com node5.example.com node6.example.com node7.example.com node8.example.com
